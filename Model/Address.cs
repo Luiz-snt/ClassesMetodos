d59a9b958eac772117d793b4223886c70d9cad11
Luiz_snt <luizhenriqued076@gmail.com>
2026-03-30 19:56:34 -0300

    Adicionar arquivos de projeto.

---
+++ Model/Address.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    internal class Address
+    {
+        public int Id { get; set; }
+        public string Street { get; set; } = string.Empty;
+        public string Number { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string PostalCode { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+        public string FederalState { get; set; } = string.Empty;
+    }
+}
---
+++ Model/Customer.cs
@@ -0,0 +1,11 @@
+﻿namespace Model
+{
+    public class Customer
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+    }
+}
